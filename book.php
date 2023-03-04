607c027f6c550bfdde2702ad3ea32e6e9403e92f
frangkyyy <126849323+frangkyyy@users.noreply.github.com>
2023-03-04 12:38:42 +0700

    Update book.php

--- book.php
+++ book.php
@@ -1,3 +1,49 @@
 <?php
-
+$link = new PDO('mysql:host=localhost;dbname=pwl01', 'root', '');
+$link->setAttribute(PDO::ATTR_AUTOCOMMIT, false);
+$link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$query = 'SELECT isbn, title, author, publisher, publish_year FROM book ';
+$stmt = $link -> prepare($query);
+$stmt->execute();
+$result = $stmt -> fetchAll();
+$link = null;
 ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="book.css">
+
+</head>
+<body>
+<h3>Book Data</h3>
+<table>
+    <thead>
+    <tr>
+        <th>ISBN</th>
+        <th>TITLE</th>
+        <th>AUTHOR</th>
+        <th>PUBLISHER</th>
+        <th>PUBLISH YEAR</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php
+    foreach ($result as $book){
+        echo '<tr>';
+        echo '<td>'. $book['isbn'] . '</td>';
+        echo '<td>'. $book['title'] . '</td>';
+        echo '<td>'. $book['author'] . '</td>';
+        echo '<td>'. $book['publisher'] . '</td>';
+        echo '<td>'. $book['publish_year'] . '</td>';
+        echo '</tr>';
+    }
+    ?>
+</body>
+</html>
+    </tbody>
+</table>
