963b9b9e4bc2e146fb6676fc02cb913244903e02
Rizky Jeremia Simanjutak <90759795+RizkySimanjuntak@users.noreply.github.com>
2023-03-04 13:35:58 +0700

    Add files via upload
    Join genre table to book table

--- book.php
+++ book.php
@@ -1,49 +1,35 @@
 <?php
-$link = new PDO('mysql:host=localhost;dbname=pwl01', 'root', '');
+$link = new PDO('mysql:host=localhost;dbname=pwl20222', 'root', '');
 $link->setAttribute(PDO::ATTR_AUTOCOMMIT, false);
 $link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$query = 'SELECT isbn, title, author, publisher, publish_year FROM book ';
+$query = 'SELECT *, genre.name FROM book JOIN genre ON book.genre_name = genre.id';
 $stmt = $link -> prepare($query);
 $stmt->execute();
 $result = $stmt -> fetchAll();
 $link = null;
 ?>
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="style.php">
 
-</head>
-<body>
-<h3>Book Data</h3>
-<table class="content-table">
+<table>
     <thead>
-    <tr>
-        <th>ISBN</th>
-        <th>TITLE</th>
-        <th>AUTHOR</th>
-        <th>PUBLISHER</th>
-        <th>PUBLISH YEAR</th>
-    </tr>
+        <tr>
+            <th>ISBN</th>
+            <th>TITLE</th>
+            <th>AUTHOR</th>
+            <th>PUBLISHER</th>
+            <th>PUBLISH YEAR</th>
+        </tr>
     </thead>
     <tbody>
-    <?php
-    foreach ($result as $book){
-        echo '<tr>';
-        echo '<td>'. $book['isbn'] . '</td>';
-        echo '<td>'. $book['title'] . '</td>';
-        echo '<td>'. $book['author'] . '</td>';
-        echo '<td>'. $book['publisher'] . '</td>';
-        echo '<td>'. $book['publish_year'] . '</td>';
-        echo '</tr>';
-    }
-    ?>
-</body>
-</html>
+        <?php
+        foreach ($result as $book){
+            echo '<tr>';
+            echo '<td>'. $book['isbn'] . '</td>';
+            echo '<td>'. $book['title'] . '</td>';
+            echo '<td>'. $book['author'] . '</td>';
+            echo '<td>'. $book['publisher'] . '</td>';
+            echo '<td>'. $book['publish_year'] . '</td>';
+            echo '</tr>';
+        }
+        ?>
     </tbody>
 </table>
