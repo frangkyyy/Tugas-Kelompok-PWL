67c024da4dd8868b6568303ed6c8d6ac5d4ac3fa
NicholasRudy2172020 <91111940+NicholasRudy2172020@users.noreply.github.com>
2023-03-05 11:18:51 +0700

    Update book.php

--- book.php
+++ book.php
@@ -21,6 +21,7 @@ $link = null;
     <script src="https://cdn.datatables.net/1.13.3/js/jquery.dataTables.min.js"></script>
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.3/css/jquery.dataTables.min.css">
     <link rel="stylesheet" type="text/css" href ="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.3/css/dataTables.bootstrap4.min.css">
 
 </head>
 <body>
