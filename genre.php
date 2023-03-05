7582356af4b01ff150784a4574ba871ff1c430d8
NicholasRudy2172020 <91111940+NicholasRudy2172020@users.noreply.github.com>
2023-03-05 21:53:36 +0700

    Update genre.php

--- genre.php
+++ genre.php
@@ -9,21 +9,16 @@ $result = $stmt -> fetchAll();
 $link = null;
 ?>
 
-<table>
-    <thead>
-        <tr>
-            <th>ID</th>
-            <th>Name</th>
-        </tr>
-    </thead>
-</table>
-<tbody>
-    <?php
-    foreach ($result as $genre) {
-        echo '<tr>';
-        echo '<td>' . $genre['id'] . '</td>';
-        echo '<td>' . $genre['name'] . '</td>';
-        echo '</tr>';
-    }
-    ?>
-</tbody>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GENRE</title>
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.13.3/js/jquery.dataTables.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.3/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" type="text/css" href ="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.3/css/dataTables.bootstrap4.min.css">
+</head>
