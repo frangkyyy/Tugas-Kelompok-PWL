e91610a1df75374c773c8111b06214e8e472e675
NicholasRudy2172020 <91111940+NicholasRudy2172020@users.noreply.github.com>
2023-03-05 21:54:49 +0700

    Update genre.php

--- genre.php
+++ genre.php
@@ -1,5 +1,5 @@
 <?php
-$link = new PDO('mysql:host=localhost;dbname=mydb', 'root', '');
+$link = new PDO('mysql:host=localhost;dbname=pwl01', 'root', '');
 $link->setAttribute(PDO::ATTR_AUTOCOMMIT, false);
 $link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $query = 'SELECT id, name FROM genre ';
