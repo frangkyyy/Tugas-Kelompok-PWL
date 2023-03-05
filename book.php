f6235f96fa44e65ca383d7a60bc95c5418608ff1
NicholasRudy2172020 <91111940+NicholasRudy2172020@users.noreply.github.com>
2023-03-05 21:45:13 +0700

    Update book.php

--- book.php
+++ book.php
@@ -15,7 +15,7 @@ $link = null;
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>BOOK</title>
     <link rel="stylesheet" href="style.php">
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script src="https://cdn.datatables.net/1.13.3/js/jquery.dataTables.min.js"></script>
