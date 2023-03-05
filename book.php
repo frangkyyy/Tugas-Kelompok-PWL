0649054bffa205150b9b08082f35968f7481889a
Rizky Jeremia Simanjutak <90759795+RizkySimanjuntak@users.noreply.github.com>
2023-03-05 21:36:27 +0700

    Update book.php

--- book.php
+++ book.php
@@ -2,7 +2,7 @@
 $link = new PDO('mysql:host=localhost;dbname=pwl20222', 'root', '');
 $link->setAttribute(PDO::ATTR_AUTOCOMMIT, false);
 $link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$query = 'SELECT isbn,title,author,publisher,publish_year FROM book JOIN genre ON genre.id = book.genre_id';
+$query = 'SELECT isbn,title,author,publisher,publish_year, genre.name FROM book JOIN genre ON genre.id = book.genre_id';
 $stmt = $link -> prepare($query);
 $stmt->execute();
 $result = $stmt -> fetchAll();
@@ -13,6 +13,7 @@ $link = null;
     <thead>
         <tr>
             <th>ISBN</th>
+            <th>NAMA GENRE</th>
             <th>TITLE</th>
             <th>AUTHOR</th>
             <th>PUBLISHER</th>
@@ -24,7 +25,7 @@ $link = null;
         foreach ($result as $book){
             echo '<tr>';
             echo '<td>'. $book['isbn'] . '</td>';
-            echo '<td>'. $book['title'] . '</td>';
+            echo '<td>'. $book['genre_id'] . '</td>';
             echo '<td>'. $book['author'] . '</td>';
             echo '<td>'. $book['publisher'] . '</td>';
             echo '<td>'. $book['publish_year'] . '</td>';
