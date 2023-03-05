b63e56a1349bdb594440c8a75e9bc8afdfcb5d59
NicholasRudy2172020 <91111940+NicholasRudy2172020@users.noreply.github.com>
2023-03-05 11:15:47 +0700

    Update book.php

--- book.php
+++ book.php
@@ -18,6 +18,7 @@ $link = null;
     <title>Document</title>
     <link rel="stylesheet" href="style.php">
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.13.3/js/jquery.dataTables.min.js"></script>
 
 </head>
 <body>
